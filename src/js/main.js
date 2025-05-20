84e05a7636dfe3779cd60afced45763d0b0d4cb6
Paula Castillo <paula_ctc@hotmail.es>
2025-05-20 19:53:24 +0200

    startimg function favorites

--- src/js/main.js
+++ src/js/main.js
@@ -28,6 +28,13 @@ let allAnimes = [];
 
 //FUNCIONES
 
+function handleclickselectanime(ev) {
+  console.log(ev.currentTarget);
+
+  const clickEventCurrentLi = ev.currentTarget;
+
+  clickEventCurrentLi.classList.toggle("favorites");
+}
 /*
 function renderOneAnimeUl() {
   renderUl.innerHTML = `<li>${infoOneAnime[0].title}</li>
@@ -35,23 +42,26 @@ function renderOneAnimeUl() {
       <img src="${infoOneAnime[0].images.jpg.image_url}"> </li>`;
 }
 
-renderOneAnimeUl();
+renderOneAnimeUl();*/
 
-const handleclickbtn = (ev) => {
+const handleclickbtnfilter = (ev) => {
   ev.preventDefault();
 
   const valueInput = inputSearch.value;
 
-  fetch(`https://api.jikan.moe/v4/anime?q=naruto ${valueInput}`);
-  then((res) => res.json());
-  then((data) => {
-    renderOneAnimeUl;
-  });
-};*/
+  fetch(`https://api.jikan.moe/v4/anime?q=${valueInput}`)
+    .then((res) => res.json())
+    .then((data) => {
+      renderUl.innerHTML = "";
+      for (const animeTitle of data.data) {
+        renderUl.innerHTML += `<li><img src="${animeTitle.images.jpg.image_url}"></li> <li>${animeTitle.title}</li>`;
+      }
+    });
+};
 
 //EVENTOS
 
-/*btnSearch.addEventListener("click", handleclickbtn);*/
+btnSearch.addEventListener("click", handleclickbtnfilter);
 
 //CUANDO CARGA LA PÁGINA
 
@@ -59,6 +69,11 @@ fetch("https://api.jikan.moe/v4/anime?q=naruto")
   .then((res) => res.json())
   .then((data) => {
     for (const animeTitle of data.data) {
-      renderUl.innerHTML += `<li><img src="${animeTitle.images.jpg.image_url}"></li> <li>${animeTitle.title}</li>`;
+      renderUl.innerHTML += `<li class="js-animelistli animelistli"><img class="img" src="${animeTitle.images.jpg.image_url}"> <h3>${animeTitle.title}</h3></li>`;
+    }
+    const allAnimeLi = document.querySelectorAll(".js-animelistli");
+
+    for (const oneAnimeLi of allAnimeLi) {
+      oneAnimeLi.addEventListener("click", handleclickselectanime);
     }
   });
